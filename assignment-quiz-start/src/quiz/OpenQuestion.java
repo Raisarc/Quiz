5a4c27ae6d0035ca090447504ad7b9087fd24a3c
Raísa Tesser Ruiz <32333493+Raisarc@users.noreply.github.com>
2021-02-18 12:52:32 +0100

    Update OpenQuestion.java

--- assignment-quiz-start/src/quiz/OpenQuestion.java
+++ assignment-quiz-start/src/quiz/OpenQuestion.java
@@ -10,12 +10,33 @@ package quiz;
  * @author raisa
  */
 public class OpenQuestion extends Question{
+    
+    private String[] questions;
+    private String[] answers;
+    private int correctAnswer;
+    
     public OpenQuestion(String question, String answer, int score){
         
     }
     
     public OpenQuestion(String question, String answer){
         
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isCorrect(String answer) {
+        answer = answer.toLowerCase();
+        return answer.equalsIgnoreCase(answers[correctAnswer]);
+    }
+
+    @Override
+    public String correctAnswer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
