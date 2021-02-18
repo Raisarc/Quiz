3a5335208eb716965485dd141a2c903015f7ce5c
Raísa Tesser Ruiz <32333493+Raisarc@users.noreply.github.com>
2021-02-18 12:51:23 +0100

    Update MultipleChoiceQuestion.java

--- assignment-quiz-start/src/quiz/MultipleChoiceQuestion.java
+++ assignment-quiz-start/src/quiz/MultipleChoiceQuestion.java
@@ -10,11 +10,41 @@ package quiz;
  * @author raisa
  */
 public class MultipleChoiceQuestion extends Question {
+    
+    private String[] questions;
+    private String[] answers;
+    private int correctAnswer;
+    
     public MultipleChoiceQuestion(String question, String[] answers, int correctAnswer, int score){
         
     }
     
     public MultipleChoiceQuestion(String question, String[] answers, int correctAnswer){
         
+    }
+    /**
+     * Prints each answer with the option item to be chosen in front
+     * @return 
+     */
+    @Override
+    public String toString() {
+        char item = 'A';
+        int value = Character.getNumericValue(item);
+        String text = "";
+        for (int i=0; i<answers.length; i++){
+            text = String.valueOf(item) + ") " + answers[0] + "\n";
+            item = (char)value++;
+        }
+        return text;
+    }
+
+    @Override
+    public boolean isCorrect(String answer) {
+        
+    }
+
+    @Override
+    public String correctAnswer() {
+        return "The correct answer is:\n"+answers[correctAnswer];
     }
 }
